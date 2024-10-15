0b28a16f60e6980c4911acd1212b0649b458d940
Kaveti Nikhil <nikhilgithub540@gmail.com>
2024-10-15 11:16:55 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,97 @@
+
+from dotenv import load_dotenv
+
+load_dotenv()
+import base64
+import streamlit as st
+import os
+import io
+from PIL import Image 
+import pdf2image
+import google.generativeai as genai
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+def get_gemini_response(input,pdf_cotent,prompt):
+    model=genai.GenerativeModel('gemini-1.5-flash')
+    response=model.generate_content([input,pdf_content[0],prompt])
+    return response.text
+
+def input_pdf_setup(uploaded_file):
+    if uploaded_file is not None:
+        ## Convert the PDF to image
+        images=pdf2image.convert_from_bytes(uploaded_file.read())
+
+        first_page=images[0]
+
+        # Convert to bytes
+        img_byte_arr = io.BytesIO()
+        first_page.save(img_byte_arr, format='JPEG')
+        img_byte_arr = img_byte_arr.getvalue()
+
+        pdf_parts = [
+            {
+                "mime_type": "image/jpeg",
+                "data": base64.b64encode(img_byte_arr).decode()  # encode to base64
+            }
+        ]
+        return pdf_parts
+    else:
+        raise FileNotFoundError("No file uploaded")
+
+## Streamlit App
+
+st.set_page_config(page_title="ATS Resume EXpert")
+st.header("ATS Tracking System")
+input_text=st.text_area("Job Description: ",key="input")
+uploaded_file=st.file_uploader("Upload your resume(PDF)...",type=["pdf"])
+
+
+if uploaded_file is not None:
+    st.write("PDF Uploaded Successfully")
+
+
+submit1 = st.button("Tell me about the resume")
+submit2 = st.button("Percentage match")
+
+# submit3 = st.button("Percentage match")
+
+input_prompt1 = """
+You are an experienced Technical Human Resource Manager specializing in talent acquisition and resume evaluation. 
+Review the uploaded resume and provide an insightful analysis, including the applicant's key strengths, areas for improvement, and relevance to the provided job description. 
+Please format your response as a professional summary.
+
+"""
+
+input_prompt2= """
+You are a skilled Applicant Tracking System (ATS) scanner with expertise in evaluating resumes against job descriptions. 
+Analyze the uploaded resume in comparison to the provided job description and give a detailed report on the percentage match. 
+Provide insights on how well the resume aligns with the job requirements and suggest any missing or misaligned qualifications.
+
+"""
+
+if submit1:
+    if uploaded_file is not None:
+        pdf_content=input_pdf_setup(uploaded_file)
+        response=get_gemini_response(input_prompt1,pdf_content,input_text)
+        st.subheader("The Repsonse is")
+        st.write(response)
+    else:
+        st.write("Please uplaod the resume")
+
+elif submit2:
+    if uploaded_file is not None:
+        pdf_content=input_pdf_setup(uploaded_file)
+        response=get_gemini_response(input_prompt2,pdf_content,input_text)
+        st.subheader("The Repsonse is")
+        st.write(response)
+    else:
+        st.write("Please uplaod the resume")
+
+
+
+   
+
+
+
+
